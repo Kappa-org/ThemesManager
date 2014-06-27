d5632ddc438adf80e05403c49b472961cd9fa36b
Budry <zarubaondra@gmail.com>
2014-06-27 12:31:43 +0200

    Added excpetions file

---
+++ src/Kappa/ThemesManager/exceptions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the Kappa\ThemesManager package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\ThemesManager;

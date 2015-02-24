e214a43f03b7f678220a91eb8cd4839c4d53f505
Budry <zarubaondra@gmail.com>
2015-02-24 09:55:54 +0100

    Added template factory

---
+++ src/Template/TemplateFactory.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of the Kappa\ThemesManager package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\ThemesManager\Template;
+
+use Kappa\ThemesManager\Theme;
+use Nette\Application\UI;
+
+/**
+ * Class TemplateFactory
+ *
+ * @package Kappa\ThemesManager\Template
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class TemplateFactory extends \Nette\Bridges\ApplicationLatte\TemplateFactory
+{
+	/** @var Theme */
+	private $theme = null;
+
+	/**
+	 * @param Theme $theme
+	 * @return $this
+	 */
+	public function setTheme(Theme $theme)
+	{
+		$this->theme = $theme;
+
+		return $this;
+	}
+
+	/**
+	 * @param UI\Control $control
+	 * @return \Nette\Bridges\ApplicationLatte\Template
+	 */
+	public function createTemplate(UI\Control $control)
+	{
+		$template = parent::createTemplate($control);
+		if ($this->theme) {
+			$template = $this->theme->getTemplateConfigurator()->configureTemplate($template);
+		}
+
+		return $template;
+	}
+}

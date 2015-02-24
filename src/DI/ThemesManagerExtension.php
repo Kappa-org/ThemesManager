6db6413df18f44273fee572f4389e633f27e2900
Budry <zarubaondra@gmail.com>
2015-02-24 11:31:22 +0100

    Added extension

---
+++ src/DI/ThemesManagerExtension.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * This file is part of the Kappa\ThemesManager package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\ThemesManager\DI;
+
+use Kappa\ThemesManager\InvalidArgumentException;
+use Kappa\ThemesManager\Mapping\PathMasksProvider;
+use Nette\DI\CompilerExtension;
+use Nette\DI\Config\Helpers;
+use Nette\DI\Statement;
+
+/**
+ * Class ThemesManagerExtension
+ *
+ * @package Kappa\ThemesManager\DI
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class ThemesManagerExtension extends CompilerExtension
+{
+	private $defaultThemeConfig = [
+		'themeDir' => null,
+		'parameters' => [],
+		'helpers' => [],
+		'macros' => [],
+		'pathMasks' => [
+			PathMasksProvider::TEMPLATES => [],
+			PathMasksProvider::LAYOUTS => []
+		]
+	];
+
+	public function loadConfiguration()
+	{
+		$config = $this->getConfig();
+		$builder = $this->getContainerBuilder();
+
+		$builder->addDefinition($this->prefix('pathMapperFactory'))
+			->setClass('Kappa\ThemesManager\Mapping\PathMapperFactory');
+
+		$registry = $builder->addDefinition($this->prefix('themeRegistry'))
+			->setClass('Kappa\ThemesManager\ThemeRegistry');
+
+		$defaultConfig = null;
+		if (array_key_exists('*', $this->config)) {
+			$defaultConfig = $config['*'];
+			unset($config['*']);
+		}
+		foreach ($config as $name => $configuration) {
+			$configuration = Helpers::merge($configuration, $this->defaultThemeConfig);
+			if ($defaultConfig) {
+				$configuration = Helpers::merge($configuration, $defaultConfig);
+			}
+			foreach ($configuration['helpers'] as $helperName => $helper) {
+				$callback = explode('::', $helper);
+				if (count($callback) != 2) {
+					throw new InvalidArgumentException("Helper '{$helper}' has no correct format. Format must be '@service::publicMethod'");
+				}
+				$configuration['helpers'][$helperName] = $callback;
+			}
+			$templateConfigurator = new Statement('Kappa\ThemesManager\Template\TemplateConfigurator', [
+				$configuration['parameters'],
+				$configuration['helpers'],
+				$configuration['macros']
+			]);
+			$pathMasksProvider = new Statement('Kappa\ThemesManager\Mapping\PathMasksProvider', [
+				$configuration['pathMasks']
+			]);
+			$theme = new Statement('Kappa\ThemesManager\Theme', [
+				$name,
+				$configuration['themeDir'],
+				$templateConfigurator,
+				$pathMasksProvider
+			]);
+			$registry->addSetup('addTheme', [$theme]);
+		}
+	}
+}
---
+++ tests/KappaTests/ThemesManager/DI/ThemesManagerExtensionTest.phpt
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of the Kappa\ThemesManager package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ *
+ * @testCase
+ */
+
+namespace Kappa\ThemesManager\Tests;
+
+use Kappa\ThemesManager\ThemeRegistry;
+use KappaTests\ThemesManager\Tests\Mocks\TestHelper;
+use Nette\Application\Application;
+use Nette\Configurator;
+use Tester\TestCase;
+use Tester\Assert;
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+/**
+ * Class ThemesManagerExtensionTest
+ *
+ * @package Kappa\ThemesManager\Tests
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class ThemesManagerExtensionTest extends TestCase
+{
+	public function testPathMapperFactory()
+	{
+		$type = 'Kappa\ThemesManager\Mapping\PathMapperFactory';
+		Assert::type($type, $this->getContainer()->getByType($type));
+	}
+
+	public function testThemeRegistry()
+	{
+		$type = 'Kappa\ThemesManager\ThemeRegistry';
+		/** @var ThemeRegistry $service */
+		$service = $this->getContainer()->getByType($type);
+		Assert::type($type, $service);
+		$theme = $service->getTheme('foo');
+		Assert::type('Kappa\ThemesManager\Theme', $theme);
+		Assert::same('foo', $theme->getName());
+		Assert::same(__DIR__, $theme->getThemeDir());
+		Assert::equal(['test'], $theme->getPathMapper()->getFormatTemplateFiles());
+		Assert::equal(['message' => 'Hello', 'themeDir' => __DIR__], $theme->getTemplateConfigurator()->getParameters());
+		Assert::equal(['helper' => [new TestHelper(), 'process']], $theme->getTemplateConfigurator()->getHelpers());
+	}
+
+	/**
+	 * @return \Nette\DI\Container
+	 */
+	private function getContainer()
+	{
+		$configurator = new Configurator();
+		$configurator->setTempDirectory(TEMP_DIR);
+		$configurator->addConfig(__DIR__ . '/../../../data/config.neon');
+
+		return $configurator->createContainer();
+	}
+}
+
+\run(new ThemesManagerExtensionTest());

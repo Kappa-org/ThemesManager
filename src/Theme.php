b3ef77f64f74e1cdac2fa020037ccdd2627aa707
Budry <zarubaondra@gmail.com>
2015-02-24 09:56:29 +0100

    Added Theme

---
+++ src/Theme.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * This file is part of the Kappa\ThemesManager package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\ThemesManager;
+
+use Kappa\ThemesManager\Mapping\PathMapper;
+use Kappa\ThemesManager\Mapping\PathMapperFactory;
+use Kappa\ThemesManager\Template\TemplateConfigurator;
+
+/**
+ * Class Theme
+ *
+ * @package Kappa\ThemesManager
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class Theme
+{
+	/** @var string */
+	private $name;
+
+	/** @var string */
+	private $themeDir;
+
+	/** @var TemplateConfigurator */
+	private $templateConfigurator;
+
+	/** @var PathMapperFactory */
+	private $pathMapperFactory;
+
+	/**
+	 * @param string $name
+	 * @param string $themeDir
+	 * @param TemplateConfigurator $templateConfigurator
+	 * @param PathMapperFactory $pathMapperFactory
+	 */
+	public function __construct($name, $themeDir, TemplateConfigurator $templateConfigurator, PathMapperFactory $pathMapperFactory)
+	{
+		if (!file_exists($themeDir) || !is_readable($themeDir)) {
+			throw new InvalidArgumentException("Theme dir '{$themeDir}' has not been found or readable");
+		}
+		$this->name = $name;
+		$this->themeDir = $themeDir;
+		$this->templateConfigurator = $templateConfigurator;
+		$this->pathMapperFactory = $pathMapperFactory;
+		$this->templateConfigurator->setParameter('themeDir', $this->themeDir);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getThemeDir()
+	{
+		return $this->themeDir;
+	}
+
+	/**
+	 * @return TemplateConfigurator
+	 */
+	public function getTemplateConfigurator()
+	{
+		return $this->templateConfigurator;
+	}
+
+	/**
+	 * @return PathMapper
+	 */
+	public function getPathMapper()
+	{
+		return $this->pathMapperFactory->create($this);
+	}
+}
---
+++ tests/KappaTests/ThemesManager/ThemeTest.phpt
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This file is part of the Kappa\ThemesManager package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ *
+ * @testCase
+ */
+
+namespace Kappa\ThemesManager\Tests;
+
+use Kappa\ThemesManager\Theme;
+use Tester\TestCase;
+use Tester\Assert;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+/**
+ * Class ThemeTest
+ *
+ * @package Kappa\ThemesManager\Tests
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class ThemeTest extends TestCase
+{
+	/** @var Theme */
+	private $theme;
+
+	protected function setUp()
+	{
+		$configuratorMock = \Mockery::mock('alias:Kappa\ThemesManager\Template\TemplateConfigurator');
+		$configuratorMock->shouldReceive('setParameter')->andReturnSelf();
+		$pathMapperFactoryMock = \Mockery::mock('Kappa\ThemesManager\Mapping\PathMapperFactory');
+		$pathMapperFactoryMock->shouldReceive('create')->andReturn(\Mockery::mock('Kappa\ThemesManager\Mapping\PathMapper'));
+		$this->theme = new Theme('foo', __DIR__, $configuratorMock, $pathMapperFactoryMock);
+	}
+
+	public function testGetName()
+	{
+		Assert::same('foo', $this->theme->getName());
+	}
+
+	public function testGetThemeDir()
+	{
+		Assert::same(__DIR__, $this->theme->getThemeDir());
+	}
+
+	public function testGetPathMapper()
+	{
+		Assert::type('Kappa\ThemesManager\Mapping\PathMapper', $this->theme->getPathMapper());
+	}
+
+	public function getTemplateConfigurator()
+	{
+		Assert::type('Kappa\ThemesManager\Template\TemplateConfigurator', $this->theme->getTemplateConfigurator());
+	}
+}
+
+\run(new ThemeTest());
